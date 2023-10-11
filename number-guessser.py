9d86377c0205595fa6d43e7e654e3dd4fe1855a5
FirozuiShaker <furouzi.abolfazl@yahoo.com>
2023-10-11 16:28:09 +0200

    which time do you get the correct answer

--- number-guessser.py
+++ number-guessser.py
@@ -14,8 +14,9 @@ else:
     quit()
         
 random_number=random.randint(0, top_of_range)
-
+guesses=0
 while True:
+    guesses +=1
     user_guess=input("Make a guess: ")
     if user_guess.isdigit():
         user_guess=int(user_guess)
@@ -28,6 +29,7 @@ while True:
         break
     else:
         print("You got wrong!")
-    
+
+print("You got it in",guesses,"guesses")
  
     
